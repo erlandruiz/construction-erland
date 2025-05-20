6804d7bc2c42bef29a1a2d04aa383a5e4eeaab71
Erland Ruiz <erlandruiz@hotmail.com>
2025-05-20 13:10:03 -0500

    second commit

--- components/ui/sheet.jsx
+++ components/ui/sheet.jsx
@@ -71,7 +71,7 @@ function SheetContent({
         {...props}>
         {children}
         <SheetPrimitive.Close
-          className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute w-11 h-11 items-center justify-center top-7 right-7 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
+          className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute w-11 h-11 items-center justify-center top-7 right-7  opacity-70 transition-opacity hover:opacity-100   focus:outline-hidden disabled:pointer-events-none">
           <XIcon className="size-7" />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
