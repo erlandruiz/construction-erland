aea6209ee26d4d111ddd66bbd98f2d1dcd622292
Erland Ruiz <erlandruiz@hotmail.com>
2025-05-24 00:22:18 -0500

    Contact finish

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,27 +1,92 @@
-import { RiChat1Line, RiMap2Line, RiSmartphoneLine } from "react-icons/ri";
+import { RiChat1Line, RiMapPin2Line, RiSmartphoneLine } from "react-icons/ri";
 import Socials from "./Socials";
 import Form from "./Form";
 const Contact = () => {
   return (
-    <div className="pt-16 xl:pt-32" id="contact">
+    <section className="pt-16 xl:pt-32" id="contact">
       <div className="container mx-auto">
         <div className="w-full xl:h-[730px] shadow-custom p-4 xl:p-8 xl:px-[90px] xl:py-[36px] border-t-4 border-bgaccent">
           <div className="flex flex-col xl:flex-row h-full gap-[40px] xl:gap-[90px]">
             {/** */}
-            <div className="w-full">INFO</div>
+            <div className="w-full xl:max-w-[380px] xl:pr-[70px] xl:border-r xl:border-border/40 h-[640px]">
+              <h4 className="text-[26px] fontprimary font-bold mb-6">
+                Contáctanos
+              </h4>
+              <p className="mb-9">
+                Estamos aquí para iluminar tu camino. Si tienes preguntas, necesitas soporte o deseas solicitar un servicio, no dudes en contactarnos. Nuestro equipo de expertos está listo para ofrecerte soluciones eléctricas eficientes y seguras.
+              </p>
+              {/** */}
+              <div className="flex flex-col gap-[40px] mb-16">
+                {/**Contac Item */}
+                <div className="flex items-start gap-[20px]">
+                  <div>
+                    <RiChat1Line className="text-[28px] textaccent" />
+                  </div>
+                  <div>
+                    <h5 className="text-[22px] font-semibold fontprimary leading-none mb-2">
+                      Chatea con Nosotros
+                    </h5>
+                    <p className="mb-4">
+                      Nuestro amable equipo está aquí para ayudarte.{" "}
+                    </p>
+                    <p className="font-semibold textprimary">
+                      empresaelectrica@mail.com
+                    </p>
+                  </div>
+                </div>
+
+                {/**Contac Item */}
+                <div className="flex items-start gap-[20px]">
+                  <div>
+                    <RiMapPin2Line className="text-[28px] textaccent" />
+                  </div>
+                  <div>
+                    <h5 className="text-[22px] font-semibold fontprimary leading-none mb-2">
+                      Oficina
+                    </h5>
+                    <p className="mb-4">Ven a saludarnos a nuestra oficina. </p>
+                    <p className="font-semibold textprimary">
+                      Jr. Los Naranjos 150, Calle Z Lt. 135
+                    </p>
+                  </div>
+                </div>
+
+                {/**Contac Item */}
+                <div className="flex items-start gap-[20px]">
+                  <div>
+                    <RiSmartphoneLine className="text-[28px] textaccent" />
+                  </div>
+                  <div>
+                    <h5 className="text-[22px] font-semibold fontprimary leading-none mb-2">
+                      Teléfono
+                    </h5>
+                    <p className="mb-4">
+                     Lunes-Viernes de 8am-5pm.
+                    </p>
+                    <p className="font-semibold textprimary">
+                      +51 999-999-999
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <Socials containerStyles="flex gap-[40px]" iconStyles="textprimary text-[20px]"/>
+            </div>
             {/**form */}
             <div className="flex-1">
-                <h2 className="h2 mb-3">Solicita una Cita.</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque asperiores nisi quibusdam quasi repellendus natus eum, vitae placeat est consequatur accusantium, praesentium maxime. Laudantium, animi? Distinctio, dolores! Hic, dignissimos molestias!
-
-                </p>
-                <Form/>
+              <h2 className="h2 mb-3">Solicita una Cita.</h2>
+              <p className="mb-9">
+                Estamos listos para ayudarte con tus necesidades. Completa el
+                siguiente formulario para programar una cita con nuestros
+                expertos. Nos pondremos en contacto contigo lo antes posible
+                para confirmar los detalles y ofrecerte una solución a medida.
+              </p>
+              <Form />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 export default Contact;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,4 +1,5 @@
-import {  Select,
+import {
+  Select,
   SelectContent,
   SelectGroup,
   SelectItem,
@@ -7,41 +8,46 @@ import {  Select,
   SelectScrollUpButton,
   SelectSeparator,
   SelectTrigger,
-  SelectValue} from '../components/ui/select'
+  SelectValue,
+} from "../components/ui/select";
 
+import { Input } from "../components/ui/input";
 
-  import {Input} from '../components/ui/input'
+import { Textarea } from "../components/ui/textarea";
 
-  import { Textarea} from "../components/ui/textarea"
-
-  import Button from './Button'
-const Form =()=>{
-    return(
-        <form>
-            <div>
-                <Input type="fullname" placeholder= "Nombre Completo"/>
-                <Input type="email" placeholder= "Correo eléctronico"/>
-                <div>
-                <Input type="phone" placeholder= "Número telefónico"/>
-                <Select>
-                    <SelectTrigger className="w-full rounded-none h-[54px] fontsecondary outline none">
-                        <SelectValue placeholder="Seleccione un Servicio"></SelectValue>
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                            <SelectLabel>Seleccione un Servicio</SelectLabel>
-                            <SelectItem value="instalaciones">Instalaciones</SelectItem>
-                            <SelectItem value="mantenimiento">Mantenimiento</SelectItem>
-                            <SelectItem value="sistemas">Sistemas</SelectItem>
-                            <SelectItem value="proyectos">Proyectos</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                </Select>
-
-                </div>
-            </div>
-        </form>
-    )
-}
+import Button from "./Button";
+const Form = () => {
+  return (
+    <form className="flex flex-col">
+      <div className="flex flex-col gap-[20px] mb-[20px]">
+        <Input type="fullname" placeholder="Nombre Completo" />
+        <Input type="email" placeholder="Correo eléctronico" />
+        <div className="flex flex-col xl:flex-row gap-[20px]">
+          <Input type="phone" placeholder="Número telefónico" />
+          <Select>
+            <SelectTrigger className="w-full rounded-none h-[54px] fontsecondary outline none">
+              <SelectValue placeholder="Seleccione un Servicio"></SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Seleccione un Servicio</SelectLabel>
+                <SelectItem value="instalaciones">Instalaciones</SelectItem>
+                <SelectItem value="mantenimiento">Mantenimiento</SelectItem>
+                <SelectItem value="sistemas">Sistemas</SelectItem>
+                <SelectItem value="proyectos">Proyectos</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <div className="flex flex-col gap-6">
+        {/** */}
+        <Textarea className="h-[180px] resize-none rounded-none" placeholder="Escriba su  mensaje"/>
+        {/** */}
+        <Button text="Enviar"/>
+      </div>
+    </form>
+  );
+};
 
 export default Form;
